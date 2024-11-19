6178f8d359bb3a0d37d992c09ec808819acd5a01
Lucas dos Anjos <lucas.anjospro@gmail.com>
2024-11-19 11:47:56 -0300

    aula 2 feita

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 import express from "express";
 
-const app = express();
 
-app.listen(3000, ()=> {
-    console.log('Server escutando...');
+// Dados falsos que está localmente na aplicação para simular uma api:
+const mock = [
+    { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
+    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150"},
+];
+
+
+// Variaveis de inicialização da aplicação Express:
+const app = express();
+app.use(express.json()); // Faz com q essa aplicação Express só responda com dados já convertidos em JSON
+const port = 3000;
+
+// Inicia servidor com Express (app):
+app.listen(port, ()=> {
+    console.log(`Server ouvindo na porta ${port}`);
 });
 
-// Rotas da API:
-app.get('/api', (req, res)=> {
-    res.status(200).send("Olá Mundo, aqui é uma API!");
+
+// Funções:
+function getIdxPostByID(idPost) 
+{
+    return mock.findIndex(post=> post.id === Number(idPost));
+}
+
+
+// Rotas/Endpoints da API:
+app.get('/posts', (req, res)=> {
+    res.status(200).json(mock);
+});
+
+app.get('/posts/:id', (req, res)=> {
+    const idx = getIdxPostByID(req.params.id);
+    res.status(200).json(mock[idx]);
 });
