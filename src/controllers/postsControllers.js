08022480a53d9c09ee0cb1108e177677ee2d09e0
Lucas dos Anjos <lucas.anjospro@gmail.com>
2024-11-25 11:32:39 -0300

    Aula 4 feita

--- src/controllers/postsControllers.js
+++ src/controllers/postsControllers.js
@@ -1,4 +1,5 @@
 import { selectAllPosts, insertPost } from "../models/postsModel.js";
+import fs from "fs";
 
 // function getIdxPostByID(idPost) 
 // {
@@ -24,6 +25,30 @@ export async function createPost(req, res)
 
     try {
         const postCreated = await insertPost(newPost);
+
+        res.status(200).json(postCreated);
+    }
+    catch(error) {
+        console.error(error);
+
+        res.status(500).json({Erro: "Falha de execução"});
+    }
+}
+
+export async function uploadImage(req, res) 
+{
+    const newPost = {
+        descricao: "",
+        url_imagem: req.file.originalname,
+        alt_imagem: ""
+    };
+
+    try {
+        const postCreated = await insertPost(newPost); //enviado no MongoDB
+
+        //Processo de modificar o nome original do arquivo vindo do client (req)
+        const pathImageRename = `uploads/${postCreated.insertedId}.png`;
+        fs.renameSync(req.file.path, pathImageRename); //Renomeia o arquivo desse caminho
 
         res.status(200).json(postCreated);
     }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,6 +1,25 @@
-import express from "express";
-import { readAllPosts, createPost } from "../controllers/postsControllers.js";
+import express from "express"; // Importa o framework Express para criar a aplicação web
+import multer from "multer"; // Importa o Multer para lidar com uploads de arquivos
+import { readAllPosts, createPost, uploadImage } from "../controllers/postsControllers.js"; // Importa as funções controladoras para lidar com a lógica dos posts
 
+// Configura o armazenamento do Multer para uploads de imagens
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      // Especifica o diretório para armazenar as imagens enviadas
+      cb(null, 'uploads/'); // Substitua por seu caminho de upload desejado
+    },
+    filename: function(req, file, cb) {
+      // Mantém o nome original do arquivo por simplicidade
+      cb(null, file.originalname); // Considere usar uma estratégia de geração de nomes únicos para produção
+    }
+});
+
+// Cria uma instância do middleware Multer
+// const upload = multer({ storage: storage });
+const upload = multer({ dest: "./uploads", storage });
+// const upload = multer({ dest:"./uploads" }); //usado em linux e mac (sem a função storage)
+
+// Define as rotas usando o objeto Express app
 const routes = (app)=> {
     app.use(express.json()); // Faz com q essa aplicação Express só responda com dados já convertidos em JSON
 
@@ -11,6 +30,9 @@ const routes = (app)=> {
 
     // Rota de criar um novo post (CREATE):
     app.post('/posts', createPost);
+
+    // Rota de uploads de imagens:
+    app.post('/upload', upload.single('file_image'), uploadImage);
 
     // app.get('/posts/:id', (req, res)=> {
     //     const idx = getIdxPostByID(req.params.id);
